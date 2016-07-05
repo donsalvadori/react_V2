2f616b280b4b1a4bfb7f180e0a171cf68b3314b9
Salvi <chris@theprogrampdx.com>
2016-07-05 16:58:37 -0700

    implemented video_list

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 
-const API_KEY = 'AIzaSyBEZsEKk2ZMOr8ijftLeT_sJgAdn8gng_4';
+const API_KEY = 'AIzaSyAcnQ33teVCilaGaUlikOUUK5WfHR-iSzg';
 
 
 class App extends Component {
